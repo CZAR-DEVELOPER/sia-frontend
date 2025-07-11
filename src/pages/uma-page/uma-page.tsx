3712cb1c6c16e17fa515b80c1b690f741c62b206
CZAR DEVELOPER <czaruribe.developer@gmail.com>
2025-07-10 21:38:13 -0600

    /Fix: Working umas

--- src/pages/uma-page/uma-page.tsx
+++ src/pages/uma-page/uma-page.tsx
@@ -60,13 +60,13 @@ const UmaPage: React.FC = () => {
     };
     try {
       await updateUmaB(payload);
-      alert("Los cambios tomarán un poco de tiempo en procesarse, recargue la página para verificar el estado en unos minutos");
+      alert("Los cambios tomarán un poco de tiempo en procesarse, se recargara automaticamente en unos segundos");
     } catch (error) {
       alert("Error al actualizar la UMA. Intente nuevamente.");
     }
     setTimeout(() => {
       window.location.reload();
-    }, 15000);
+    }, 25000);
   };
   
 
@@ -322,13 +322,13 @@ const UmaPage: React.FC = () => {
             <div>
               <AvatarComponent variant="white" size="large">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  viewBox="0 0 16 16">
-  <path d="M9.5 12.5a1.5 1.5 0 1 1-2-1.415V6.5a.5.5 0 0 1 1 0v4.585a1.5 1.5 0 0 1 1 1.415"/>
-  <path d="M5.5 2.5a2.5 2.5 0 0 1 5 0v7.55a3.5 3.5 0 1 1-5 0zM8 1a1.5 1.5 0 0 0-1.5 1.5v7.987l-.167.15a2.5 2.5 0 1 0 3.333 0l-.166-.15V2.5A1.5 1.5 0 0 0 8 1"/>
+  <path fill-rule="evenodd" d="M7.21.8C7.69.295 8 0 8 0q.164.544.371 1.038c.812 1.946 2.073 3.35 3.197 4.6C12.878 7.096 14 8.345 14 10a6 6 0 0 1-12 0C2 6.668 5.58 2.517 7.21.8m.413 1.021A31 31 0 0 0 5.794 3.99c-.726.95-1.436 2.008-1.96 3.07C3.304 8.133 3 9.138 3 10c0 0 2.5 1.5 5 .5s5-.5 5-.5c0-1.201-.796-2.157-2.181-3.7l-.03-.032C9.75 5.11 8.5 3.72 7.623 1.82z"/>
+  <path fill-rule="evenodd" d="M4.553 7.776c.82-1.641 1.717-2.753 2.093-3.13l.708.708c-.29.29-1.128 1.311-1.907 2.87z"/>
 </svg>
               </AvatarComponent>
             </div>
             <div>
-              <h3 className="text">Temperatura de inyección</h3>
+              <h3 className="text">Apertura de agua helada</h3>
               <p className="text-sm opacity-50">
               <input
                 type="number"
@@ -404,14 +404,6 @@ const UmaPage: React.FC = () => {
             </ButtonComponent>
             
             
-            <ButtonComponent
-              className="mt-4 w-full opacity-75"
-              style="icon"
-              size="sm"
-              onClick={() => window.location.reload()}
-            >
-              Recargar página
-            </ButtonComponent>
 
             <p className="text-sm text-gray-500 mt-2 text-center">
               El status de la UMA se actualizará en unos minutos despues de enviada la solicitud. Si no ves cambios espera unos minutos y recarga la página
